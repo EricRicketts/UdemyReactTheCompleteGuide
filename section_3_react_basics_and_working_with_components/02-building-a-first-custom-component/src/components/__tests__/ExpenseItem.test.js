3b4cae8ac7dd5b4ac28d334805fbd6a23785ff62
Eric Ricketts <eric_ricketts@me.com>
2022-09-18 18:55:20 -0400

    ExpenseItem and ExpenseDate

--- section_3_react_basics_and_working_with_components/02-building-a-first-custom-component/src/components/__tests__/ExpenseItem.test.js
+++ section_3_react_basics_and_working_with_components/02-building-a-first-custom-component/src/components/__tests__/ExpenseItem.test.js
@@ -3,10 +3,38 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ExpenseItem from "../ExpenseItem";
 
-describe('Test ExpenseItem component', () => {
-  test('ExpenseItem component should include its own h2 element', () => {
-    render(<ExpenseItem />);
-    expect(screen.getByRole("heading", { level: 2 })).toBeInTheDocument();
+describe("Test ExpenseItem component", () => {
+  let data;
+  beforeEach(() => {
+    data = {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    };
   });
+  test("ExpenseItem component should include its own h2 element", () => {
+    render(
+      <ExpenseItem title={data.title} date={data.date} amount={data.amount} />
+    );
+    const heading = screen.getByRole("heading", { level: 2 });
+    expect(heading).toHaveTextContent("Car Insurance");
+  });
+  test('ExpenseItem component should inclue item price', () => {
+    render(
+      <ExpenseItem title={data.title} date={data.date} amount={data.amount} />
+    );
+    expect(screen.getByText("$294.67")).toBeInTheDocument();
+  });
+  test('should have all date items', () => {
+    const expected = [/March/i, /28/, /2021/];
+    const date = new Date(2021, 2, 28); 
+    render(
+      <ExpenseItem title={data.title} date={data.date} amount={data.amount} />
+    );
+    expected.forEach(dateItem => {
+      expect(screen.getByText(dateItem)).toBeInTheDocument();
+    });
     
+  });
 });
