66fcfee6a44884f83d1432522458113384dd1d5f
Eric Ricketts <eric_ricketts@me.com>
2022-09-24 06:06:25 -0400

    reorganize directories to better follow the syllabus making it clear which part of the course is being studied

---
+++ section_4_react_state_and_working_with_events/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = props => {
+  const amount = '$' + props.amount;
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ section_4_react_state_and_working_with_events/src/components/UI/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+const Card = props => {
+  // white space is needed for concatenating classes together
+  const classes = "card " + props.className;
+  return (
+    <div className={classes}>{props.children}</div>
+  ) 
+}
+
+export default Card;
