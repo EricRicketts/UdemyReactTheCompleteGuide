5cefc6bff0b000717c5fbdf8b46f75f08b3456c8
Eric Ricketts <eric_ricketts@me.com>
2022-09-24 09:47:45 -0400

    explain why react does not rerender when only a local variable is updated

--- section_4_react_state_and_working_with_events/src/components/Expenses/ExpenseItem.js
+++ section_4_react_state_and_working_with_events/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,25 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const ExpenseItem = props => {
+  let title = props.title;
   const amount = '$' + props.amount;
-  const clickHandler = () => console.log("Clicked!!!");
+  const clickHandler = () => {
+    const titles = ["Foo", "Bar", "Fizz", "Buzz"];
+    console.log("Clicked!!!");
+    console.log("title was: " + title);
+    const index = Math.floor(Math.random() * titles.length)
+    title = titles[index];
+    console.log("title is now: " + title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
