6039b0fcd20f4ce164e9dcf2997befc1df82dede
Eric Ricketts <eric_ricketts@me.com>
2022-09-25 09:28:19 -0400

    test enter new title on form

--- section_4_react_state_and_working_with_events/src/components/__tests__/NewExpense.test.js
+++ section_4_react_state_and_working_with_events/src/components/__tests__/NewExpense.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NewExpense from "../NewExpense/NewExpense";
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 describe("Test New Expense", () => {
@@ -18,5 +19,13 @@ describe("Test New Expense", () => {
       await waitFor(() => expect(screen.queryByLabelText("Date")).toBeInTheDocument());
     }); 
   });
-  
+  describe("Test That Data Can Be Entered In The Form", () => {
+    test("should be able to enter a new title", async () => {
+      render(<NewExpense />); 
+      const titleInput = await screen.findByLabelText("Title"); 
+      const newTitle = "New Title"
+      await userEvent.type(titleInput, newTitle);
+      await waitFor(() => expect(titleInput).toHaveValue(newTitle));
+    }); 
+  }); 
 });
