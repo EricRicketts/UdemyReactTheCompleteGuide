c8be89a019ae0a99a847a4c87019fbb498654d77
Eric Ricketts <eric_ricketts@me.com>
2022-10-06 17:23:16 -0400

    clean up ExpenseItem.js so it is presentational only

--- section_4_react_state_and_working_with_events/src/components/Expenses/ExpenseItem.js
+++ section_4_react_state_and_working_with_events/src/components/Expenses/ExpenseItem.js
@@ -4,23 +4,14 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 
 const ExpenseItem = props => {
-  const [title, setTitle] = useState(props.title);
-  const amount = '$' + props.amount;
-  const clickHandler = () => {
-    const titles = ["Foo", "Bar", "Fizz", "Buzz"];
-    const index = Math.floor(Math.random() * titles.length)
-    setTitle(titles[index]);
-    console.log("title is now: " + title);
-  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
