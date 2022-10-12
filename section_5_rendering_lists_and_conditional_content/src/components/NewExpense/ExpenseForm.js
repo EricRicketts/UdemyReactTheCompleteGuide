8a42b71423de2a82a400a1db3a921ac48519b28c
Eric Ricketts <eric_ricketts@me.com>
2022-10-12 12:48:40 -0400

    had to replace my code with instructor code because the chart was not rendering months with added expenses correctly

--- section_5_rendering_lists_and_conditional_content/src/components/NewExpense/ExpenseForm.js
+++ section_5_rendering_lists_and_conditional_content/src/components/NewExpense/ExpenseForm.js
@@ -1,102 +1,94 @@
-import React, { useState } from "react";
-import "./ExpenseForm.css";
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
-  const [addNewExpense, setAddNewExpense] = useState(false);
-
-  const cancelExpense = () => {
-    toggleAddNewExpenseToFalse();
-    setEnteredAmount("");
-    setEnteredDate("");
-    setEnteredTitle("");
-  };
-
-  const toggleAddNewExpenseToTrue = () => {
-    setAddNewExpense((previous) => true);
-  };
-
-  const toggleAddNewExpenseToFalse = () => {
-    setAddNewExpense((previous) => false);
-  };
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
+
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
+
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    props.onSaveExpenseData(expenseData);
-    toggleAddNewExpenseToFalse();
-    setEnteredAmount("");
-    setEnteredDate("");
-    setEnteredTitle("");
-  };
 
-  if (!addNewExpense) {
-    return (
-      <button
-        className="new-expense button"
-        onClick={toggleAddNewExpenseToTrue}
-      >
-        Add New Expense
-      </button>
-    );
-  }
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label id="form-title">Title</label>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
           <input
-            aria-labelledby="form-title"
-            type="text"
+            type='text'
             value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <label id="form-amount">Amount</label>
+        <div className='new-expense__control'>
+          <label>Amount</label>
           <input
-            aria-labelledby="form-amount"
-            type="number"
+            type='number'
+            min='0.01'
+            step='0.01'
             value={enteredAmount}
-            min="0.01"
-            step="0.01"
             onChange={amountChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <label id="form-date">Date</label>
+        <div className='new-expense__control'>
+          <label>Date</label>
           <input
-            aria-labelledby="form-date"
-            type="date"
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
             value={enteredDate}
-            min="2019-01-01"
-            max="2022-12-31"
             onChange={dateChangeHandler}
           />
         </div>
       </div>
-      <div className="new-expenses__actions">
-        <button type="button" onClick={cancelExpense}>
-          Cancel
-        </button>
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
--- section_5_rendering_lists_and_conditional_content/src/components/UI/Card.js
+++ section_5_rendering_lists_and_conditional_content/src/components/UI/Card.js
@@ -1,11 +1,11 @@
-import "./Card.css";
+import React from 'react';
 
-const Card = props => {
-  // white space is needed for concatenating classes together
-  const classes = "card " + props.className;
-  return (
-    <div className={classes}>{props.children}</div>
-  ) 
-}
+import './Card.css';
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+
+  return <div className={classes}>{props.children}</div>;
+};
 
 export default Card;
