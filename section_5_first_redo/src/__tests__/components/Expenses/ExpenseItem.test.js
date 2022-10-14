9016c1720a08101eb0206163fc8b10ff7e3aabef
EricRicketts <eric_ricketts@me.com>
2022-10-14 08:50:04 -0400

    remove ExpenseDate.test.js as that component's rendering is covered in ExpenseItem.test.js

---
+++ section_5_first_redo/src/__tests__/components/Expenses/ExpenseItem.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ExpenseItem from "../../../components/Expenses/ExpenseItem";
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Test The ExpenseItem Component", () => {
+  let expense;
+  beforeEach(() => {
+    expense = {
+      id: "#4C9",
+      title: "Test Item",
+      amount: 45.32,
+      date: new Date("12/10/2019"),
+    };
+  });
+
+  describe("Test The ExpenseDate Component Renders", () => {
+    test("should render the day of the month", () => {
+      render(
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+      expect(screen.getByText("10")).toBeInTheDocument();
+    });
+    test("should render the full month name", () => {
+      render(
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+      expect(screen.getByText("December")).toBeInTheDocument();
+    });
+    test("should render the full year", () => {
+      render(
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+      expect(screen.getByText("2019")).toBeInTheDocument();
+    });
+  });
+});
---
+++ section_5_first_redo/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ExpeneseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpeneseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
